bc9d2fe85d402dd65cf3e418bc028ccc54d7c445
itu-itis21-nguyen21 <nguyen21@itu.edu.tr>
2021-11-01 22:58:43 +0300

    Finished chap 4: Interactive prompt (plz ignore the previous commit)

--- learning.c
+++ learning.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#include <editline/readline.h>
-#include <editline/history.h>
+/* Declare a buffer for user input of size 2048 */
+static char input[2048];
 
-int main (int argc, char**argv) {
-	
+int main(int argc, char** argv) {
+
 	/* Print Version and Exit Information */
-	puts("Lispy Version 0.0.0.0.1"); 
+	puts("Lispy Version 0.0.0.0.1");
 	puts("Press Ctrl+c to Exit\n");
-	
+
 	/* In a never ending loop */
 	while (1) {
-		
-		/* Output our prompt and get input */
-		char* input = readline(" lispy> ");
-		
-		/* Add input to history */
-		add_history(input);
-		
-		/* Echo input back to user */
-		printf("No you're a %s", input);
-		
-		/* Free input */
-		free(input);
+
+	/* Output our prompt */
+	fputs("lispy> ", stdout);
+
+	/* Read a line of user input of maximum size 2048 */
+	fgets(input, 2048, stdin);
+
+	/* Echo input back to user */
+	printf("No you're a %s", input);
 	}
-	
+
 	return 0;
 }
